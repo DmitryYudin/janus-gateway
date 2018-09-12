2fa535449252f199013c67ac740e607590bd908f
Dima <yudind@gmail.com>
2018-09-12 17:41:17 +0300

    configured under Ubuntu
    set -e
    ./autogen.sh
    ./configure \
        --disable-rest \
        --disable-rabbitmq \
        --disable-mqtt \
        --disable-unix-sockets \
        --disable-nanomsg \
        --disable-sample-event-handler \
        --disable-rabbitmq-event-handler \
        --disable-plugin-audiobridge \
        --disable-plugin-duktape \
        --disable-plugin-echotest \
        --disable-plugin-lua \
        --disable-plugin-recordplay \
        --disable-plugin-sip \
        --disable-plugin-sipre \
        --disable-plugin-streaming \
        --disable-plugin-textroom \
        --disable-plugin-videocall \
        --disable-plugin-videoroom \
        --disable-plugin-voicemail
    
    make V=1

---
+++ src/version.c
@@ -0,0 +1,5 @@
+#include "version.h"
+const char *janus_build_git_sha = "ef8477e6081c4015e244fbce37d9930e73b83412";
+const char *janus_build_git_time = "Чт авг 16 19:30:30 MSK 2018";
+int janus_version = 43;
+const char *janus_version_string = "0.4.3";
