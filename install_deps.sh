86fab4a1f41d552268d1e506e1b8c03bec7d530c
Dima <yudind@gmail.com>
2019-01-24 13:31:53 +0300

    ubuntu deps

---
+++ install_deps.sh
@@ -0,0 +1,17 @@
+apt="sudo apt -q -y"
+$apt install \
+		gettext-* \
+		glib2-* \
+		gengetopt \
+		libconfig-dev \
+		libjansson-dev \
+		openssl-* \
+		paho-mqtt-* \
+		libmicrohttpd-* \
+		librabbitmq-* \
+		libnice-dev \
+		libsrtp2-dev \
+		libwebsockets-dev
+
+#		gnutls-*
+#		libcurl-*
