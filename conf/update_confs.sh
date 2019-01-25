2b8fb75afb77f58a54110d89e52c36187660e68a
Dima <yudind@gmail.com>
2019-01-25 12:57:38 +0300

    +set_config from docker build

--- conf/update_confs.sh
+++ conf/update_confs.sh
@@ -1,11 +1,20 @@
 set -e
 
-prefix=/usr/local
+export JANUS_DEBUG_LEVEL=4
+export JANUS_WS_PORT=8090
+export JANUS_ICE_LOCAL_IP=
+export JANUS_ICE_PORT_RANGE=
+export JANUS_TURN_REST_API=
+export JANUS_TURN_SERVER=
+export JANUS_TURN_PORT=3456
+export JANUS_NOSIP_LOCAL_IP=
+export JANUS_NOSIP_PORT_RANGE=
+export JANUS_LS_HTTP_HOST=
+export JANUS_LS_HTTP_PORT=
+export JANUS_PROMETHEUS_HOST=0.0.0.0
+export JANUS_PROMETHEUS_PORT=9091
 mkdir -p $prefix/etc/janus
-cp -f ./janus/janus.jcfg $prefix/etc/janus/
-cp -f ./janus/janus.plugin.nosip.jcfg  $prefix/etc/janus/
-cp -f ./janus/janus.transport.websockets.jcfg  $prefix/etc/janus/
-cp -f ./janus/janus.eventhandler.rabbitmqevh.jcfg $prefix/etc/janus/
+./config/set_config.sh
 
 prefix=/usr/local
 mkdir -p $prefix/share/janus/certs
